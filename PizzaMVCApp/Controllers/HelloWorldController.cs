5ee8be77edb5fb1e37f1b5927055da37ee078ad7
Sasanka SG <sasankasg697@gmail.com>
2024-10-08 11:06:03 +0530

    first commit

---
+++ PizzaMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaMVCApp.Models;
+
+namespace PizzaMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel model = new DogViewModel() { Name = "BUBBLES", Age= 5 };
+            return View(model);
+        }
+
+        public string Hello()
+        {
+            return "Hi There! Welcome page!";
+        }
+    }
+}
---
+++ PizzaMVCApp/Models/DogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PizzaMVCApp.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
